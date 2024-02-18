c7cd9fa1abceb1fc1b939069ef609d5831fb98d2
DenisaTerchescu <dterchescu@yahoo.com>
2024-02-18 17:37:51 +0200

    Created the graphics pipeline yay

--- resources/shaders/compile_simple.bat
+++ resources/shaders/compile_simple.bat
@@ -1,6 +1,6 @@
 
 set BASE_PATH=%~dp0
 
-D:\VulkanSDK\1.3.275.0\Bin\glslc.exe %BASE_PATH%shader.vert -o %BASE_PATH%shader.vert.spv
-D:\VulkanSDK\1.3.275.0\Bin\glslc.exe %BASE_PATH%shader.frag -o %BASE_PATH%shader.frag.spv
+D:\VulkanSDK\1.3.275.0\Bin\glslc.exe %BASE_PATH%shader.vert -o %BASE_PATH%vert.spv
+D:\VulkanSDK\1.3.275.0\Bin\glslc.exe %BASE_PATH%shader.frag -o %BASE_PATH%frag.spv
 pause
