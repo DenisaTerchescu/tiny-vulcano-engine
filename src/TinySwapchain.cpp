2b9bb807658880b159e22e75b1eed3fa299fb150
DenisaTerchescu <Denisa@DESKTOP-FTJGMRG>
2024-12-06 13:08:05 +0200

    Enabled v sync

--- src/TinySwapchain.cpp
+++ src/TinySwapchain.cpp
@@ -144,7 +144,8 @@ VkSurfaceFormatKHR TinySwapChain::chooseSwapSurfaceFormat(const std::vector<VkSu
 
 VkPresentModeKHR TinySwapChain::chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes) {
     for (const auto& availablePresentMode : availablePresentModes) {
-        if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
+        //if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR ) {
+        if (availablePresentMode == VK_PRESENT_MODE_FIFO_KHR) {
             return availablePresentMode;
         }
     }
