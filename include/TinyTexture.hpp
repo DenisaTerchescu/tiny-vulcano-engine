33cf2040e01bdb130192dc3814c4a16b065e6a90
DenisaTerchescu <Denisa@DESKTOP-FTJGMRG>
2025-04-20 18:37:24 +0300

    Added new textures yay

--- include/TinyTexture.hpp
+++ include/TinyTexture.hpp
@@ -7,7 +7,9 @@
 #include "TinyBuffer.hpp"
 #include "TinyCommand.hpp"
 
-const std::string TEXTURE_PATH = RESOURCES_PATH "textures/glass.png";
+const std::string GLASS_TEXTURE_PATH = RESOURCES_PATH "textures/glass.png";
+//const std::string PINK_TEXTURE_PATH = RESOURCES_PATH "textures/pink.jpg";
+//const std::string WOOD_TEXTURE_PATH = RESOURCES_PATH "textures/wood.jpg";
 
 class TinyTexture{
 public:
